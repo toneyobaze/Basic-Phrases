79cb7d1193bd5763c20743871c53c22cb032ae87
Anthony <you@example.comcls>
2018-09-27 12:35:13 +0000

    Initial commit

---
+++ app/src/main/java/com/example/oladapo/basicphrases/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.oladapo.basicphrases;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void ButtonTapped(View view){
+
+        int id =view.getId();
+        String ourId;
+
+        ourId = view.getResources().getResourceEntryName(id);
+
+        int resourceId = getResources().getIdentifier(ourId, "raw", "com.example.oladapo.basicphrases");
+
+        MediaPlayer mplayer = MediaPlayer.create(this, resourceId);
+        mplayer.start();
+
+        Log.i("button tapped", ourId);
+
+    }
+}
